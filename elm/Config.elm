85b918e3a69f8967a96e835d9f1188b7e3877bc2
Intimidator <hanno@Hanno.co.za>
2020-10-10 21:22:34 +0200

    First version?

---
+++ elm/Config.elm
@@ -0,0 +1,60 @@
+module Config exposing (..)
+
+import BigInt exposing (BigInt)
+import Eth.Types exposing (Address)
+import Eth.Utils
+import Time
+import TokenValue exposing (TokenValue)
+
+
+testMode =
+    False
+
+
+httpProviderUrl : String
+httpProviderUrl =
+    if testMode then
+        ganacheHttpProviderUrl
+
+    else
+        mainnetHttpProviderUrl
+
+
+mainnetHttpProviderUrl : String
+mainnetHttpProviderUrl =
+    "https://23eb406fad764a70987ba5e619459917.eth.rpc.rivet.cloud/"
+
+
+ganacheHttpProviderUrl : String
+ganacheHttpProviderUrl =
+    "http://localhost:8545"
+
+
+daiContractAddress : Address
+daiContractAddress =
+    if testMode then
+        Eth.Utils.unsafeToAddress "0xCfEB869F69431e42cdB54A4F4f105C19C080A601"
+
+    else
+        Eth.Utils.unsafeToAddress "0x6B175474E89094C44Da98b954EedeAC495271d0F"
+
+etherscanBaseUrl: String
+etherscanBaseUrl = "https://etherscan.io/address/"
+
+teamToastMultiSigAddress: Address
+teamToastMultiSigAddress = Eth.Utils.unsafeToAddress "0xE5dDe1cc679184fc420E6f92e0Bd8C81E41D25e1"
+
+
+treasuryForwarderAddress: Address
+treasuryForwarderAddress = Eth.Utils.unsafeToAddress "0x93fE7D1d24bE7CB33329800ba2166f4D28Eaa553"
+
+
+fryTokenAddress : Address
+fryTokenAddress = Eth.Utils.unsafeToAddress "0x6c972b70c533E2E045F333Ee28b9fFb8D717bE69"
+
+
+bucketSaleAddress : Address
+bucketSaleAddress = Eth.Utils.unsafeToAddress "0x30076fF7436aE82207b9c03AbdF7CB056310A95A"
+
+uniswapPoolLink : String
+uniswapPoolLink = "https://uniswap.info/token/0x6c972b70c533e2e045f333ee28b9ffb8d717be69"
--- elm/View.elm
+++ elm/View.elm
@@ -1,9 +1,6 @@
 module View exposing (root)
 
 import Browser
-import Common.Msg exposing (..)
-import Common.Types exposing (..)
-import Common.View exposing (..)
 import Dict exposing (Dict)
 import Dict.Extra
 import Element exposing (Attribute, Element)
@@ -21,24 +18,18 @@ import Helpers.Eth as EthHelpers
 import Helpers.List as ListHelpers
 import Helpers.Time as TimeHelpers
 import Helpers.Tuple as TupleHelpers
-import Home.View as Home
 import Html.Attributes
 import Images
 import Json.Decode
 import List.Extra
 import Maybe.Extra
 import MaybeDebugLog exposing (maybeDebugLog)
-import Phace
-import Routing exposing (Route)
-import Sentiment.View as Sentiment
-import Stats.View as Stats
 import Theme exposing (defaultTheme)
 import Time
 import TokenValue exposing (TokenValue)
 import Tuple3
 import Types exposing (..)
-import UserNotice as UN exposing (UserNotice)
-import Wallet exposing (Wallet)
+
 
 
 root : Model -> Browser.Document Msg
@@ -47,231 +38,97 @@ root model =
     , body =
         [ Element.layout
             ([ Element.width Element.fill
+             , Element.Background.color Theme.blue
              , Element.htmlAttribute <| Html.Attributes.style "height" "100vh"
-             , Element.Events.onClick ClickHappened
              , Element.Font.family
                 [ Element.Font.typeface "DM Sans"
-                , Element.Font.sansSerif
+                , Element.Font.sansSerif                
                 ]
+             , Element.Font.color EH.white
              ]
-                ++ List.map Element.inFront (modals model)
             )
           <|
             body model
         ]
     }
 
-
-modals : Model -> List (Element Msg)
-modals model =
-    Maybe.Extra.values
-        ([]
-            ++ List.map Just
-                (userNoticeEls
-                    model.dProfile
-                    model.userNotices
-                )
-        )
-
-
 body : Model -> Element Msg
 body model =
-    Element.column
-        [ Element.width Element.fill
-        , Element.Background.color defaultTheme.appBackground
-        , Element.height Element.fill
-        ]
-        [ header
-            model.dProfile
-            model.submodel
-            (Wallet.userInfo model.wallet)
-        , case model.submodel of
-            Home homeModel ->
-                Element.map HomeMsg <|
-                    Home.view
-                        model.dProfile
-                        homeModel
-                        (Wallet.userInfo model.wallet)
-
-            Sentiment sentimentModel ->
-                Element.map SentimentMsg <|
-                    Sentiment.view
-                        model.dProfile
-                        sentimentModel
-                        (Wallet.userInfo model.wallet)
-
-            Stats statsModel ->
-                Element.map StatsMsg <|
-                    Stats.view
-                        model.dProfile
-                        statsModel
-                        (Wallet.userInfo model.wallet)
-        ]
-
-
-header : EH.DisplayProfile -> Submodel -> Maybe UserInfo -> Element Msg
-header dProfile mode walletUXPhaceInfo =
-    Element.row
-        [ Element.width Element.fill
-        , Element.Background.color defaultTheme.headerBackground
-        , Element.padding (20 |> changeForMobile 10 dProfile)
-        , Element.spacing (10 |> changeForMobile 5 dProfile)
-        , Element.Border.glow
-            (EH.black |> EH.withAlpha 0.5)
-            5
-        ]
-        [ case dProfile of
-            Mobile ->
-                Element.el [ Element.alignTop, Element.alignLeft ] <| logoBlock dProfile
-
-            Desktop ->
-                logoBlock dProfile
-        , Element.el
-            [ Element.centerY
-            , Element.alignRight
-            ]
-          <|
-            EH.forgedByFoundry dProfile
-        ]
-
-
-logoBlock : EH.DisplayProfile -> Element Msg
-logoBlock dProfile =
-    Element.row
-        [ Element.height Element.fill
-        , Element.padding 10
-        , Element.spacing 20
-        ]
-        [ Images.toElement
-            [ Element.centerY
-            , Element.width <| Element.px 60
-            ]
-            Images.fryIcon
-        , Element.column
-            [ Element.spacing 5 ]
-            [ Element.el
-                [ Element.Font.color EH.white
-                , Element.Font.size 35
-                , Element.Font.bold
-                , Element.centerY
-                ]
-              <|
-                Element.text "Foundry Dashboard"
-            , Element.newTabLink
-                [ Element.alignLeft
-                , Element.Background.color Theme.blue
-                , Element.paddingXY 10 3
-                , Element.Border.rounded 4
-                , Element.Font.color EH.white
-                , Element.Font.size 18
-                ]
-                { url = "https://foundrydao.com"
-                , label =
-                    Element.text "What is Foundry?"
-                }
-            ]
-        ]
-
-
-userNoticeEls : EH.DisplayProfile -> List UserNotice -> List (Element Msg)
-userNoticeEls dProfile notices =
-    if notices == [] then
-        []
-
-    else
-        [ Element.column
-            [ Element.moveLeft (20 |> EH.changeForMobile 5 dProfile)
-            , Element.moveUp (20 |> EH.changeForMobile 5 dProfile)
-            , Element.spacing (10 |> EH.changeForMobile 5 dProfile)
-            , Element.alignRight
-            , Element.alignBottom
-            , Element.width <| Element.px (300 |> EH.changeForMobile 150 dProfile)
-            , Element.Font.size (15 |> EH.changeForMobile 10 dProfile)
-            ]
-            (notices
-                |> List.indexedMap (\id notice -> ( id, notice ))
-                |> List.filter (\( _, notice ) -> notice.align == UN.BottomRight)
-                |> List.map (userNotice dProfile)
-            )
-        , Element.column
-            [ Element.moveRight (20 |> EH.changeForMobile 5 dProfile)
-            , Element.moveDown 100
-            , Element.spacing (10 |> EH.changeForMobile 5 dProfile)
-            , Element.alignLeft
-            , Element.alignTop
-            , Element.width <| Element.px (300 |> EH.changeForMobile 150 dProfile)
-            , Element.Font.size (15 |> EH.changeForMobile 10 dProfile)
-            ]
-            (notices
-                |> List.indexedMap (\id notice -> ( id, notice ))
-                |> List.filter (\( _, notice ) -> notice.align == UN.TopLeft)
-                |> List.map (userNotice dProfile)
-            )
-        ]
-
-
-userNotice : EH.DisplayProfile -> ( Int, UserNotice ) -> Element Msg
-userNotice dProfile ( id, notice ) =
-    let
-        color =
-            case notice.noticeType of
-                UN.Update ->
-                    Element.rgb255 100 200 255
-
-                UN.Caution ->
-                    Element.rgb255 255 188 0
-
-                UN.Error ->
-                    Element.rgb255 255 70 70
-
-                UN.ShouldBeImpossible ->
-                    Element.rgb255 200 200 200
-
-        textColor =
-            case notice.noticeType of
-                UN.Error ->
-                    Element.rgb 1 1 1
-
-                _ ->
-                    Element.rgb 0 0 0
-
-        closeElement =
-            EH.closeButton
-                [ Element.alignRight
-                , Element.alignTop
-                , Element.moveUp 2
-                ]
-                EH.black
-                (DismissNotice id)
-    in
-    Element.el
-        [ Element.Background.color color
-        , Element.Border.rounded (10 |> EH.changeForMobile 5 dProfile)
-        , Element.padding (8 |> EH.changeForMobile 3 dProfile)
+    Element.column 
+        [ Element.Background.color Theme.softRed
         , Element.width Element.fill
-        , Element.Border.width 1
-        , Element.Border.color <| Element.rgba 0 0 0 0.15
-        , EH.subtleShadow
-        , EH.onClickNoPropagation <| MsgUp NoOp
+        , Element.Border.rounded 50
         ]
-        (notice.mainParagraphs
-            |> List.map (List.map (Element.map never))
-            |> List.indexedMap
-                (\pNum paragraphLines ->
-                    Element.paragraph
-                        [ Element.width Element.fill
-                        , Element.Font.color textColor
-                        , Element.spacing 1
-                        ]
-                        (if pNum == 0 then
-                            closeElement :: paragraphLines
-
-                         else
-                            paragraphLines
-                        )
-                )
-            |> Element.column
-                [ Element.spacing 4
-                , Element.width Element.fill
+        [ Element.row
+            [ Element.paddingEach { left = 15, top = 0, right = 0, bottom = 0}]
+            [ textExtraLarge "BUY FRY"
+            , Element.column
+                [ Element.padding 20 ]
+                [ textSmall "BUCKET #"
+                , textLarge "[bucketNr]"
                 ]
-        )
+            , Element.column
+                [ Element.padding 20 ]
+                [ textSmall "TIME LEFT"
+                , textLarge "99H59M59S"
+                ]
+            , Element.column
+                [ ]
+                [ textSmall "PRICE"
+                , Element.row
+                    []
+                    [ Element.column
+                        [ Element.padding 5 ]
+                        [ textLarge "SALE"
+                        , textLarge "UNISWAP"
+                        ]
+                    , Element.column
+                        [ Element.padding 5
+                        , Element.alignRight 
+                        ]
+                        (columnItems "0.00002" "0.00001")
+                    ]
+                ]
+            ]
+        ]
+
+textExtraLarge : String -> Element Msg
+textExtraLarge txt =
+    Element.el 
+        [ Element.Font.size 32
+        , Element.padding 5
+        , Element.Font.bold ]
+     <|
+        Element.text txt
+
+
+textLarge : String -> Element Msg
+textLarge txt = 
+    Element.el
+        [ Element.Font.size 16
+        , Element.padding 5 ]
+     <|
+        Element.text txt        
+
+textSmall : String -> Element Msg
+textSmall txt = 
+    Element.el
+        [ Element.Font.size 8
+        , Element.padding 5 ]
+     <|
+        Element.text txt
+
+columnLeft : String -> String -> Int -> Element Msg
+columnLeft str1 str2 padding =
+    Element.column
+        [ Element.padding padding ]
+        [ textLarge str1
+        , textLarge str2
+        ]
+
+
+columnItems : String -> String -> List(Element Msg)
+columnItems str1 str2 =
+        [ textLarge str1
+        , textLarge str2
+        ]
